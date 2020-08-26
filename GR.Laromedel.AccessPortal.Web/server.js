32e76dedbae3ac098e9be5882c303dff80560744
Adam Näslund <adam.naslund@iteam.se>
2020-08-26 14:31:30 +0200

    feat: start work on node React site

--- GR.Laromedel.AccessPortal.Web/server.js
+++ GR.Laromedel.AccessPortal.Web/server.js
@@ -1,8 +1,10 @@
-'use strict';
-var http = require('http');
 var port = process.env.PORT || 1337;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(port)
