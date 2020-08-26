e4b91b6699d3c16d5f4aacf923db41454318ab90
Andreas Indal <andreas.indal@iteam.se>
2020-08-26 15:45:26 +0200

    Add basic functionality for fetching and rendering licenses

--- GR.Laromedel.AccessPortal.Web/index.js
+++ GR.Laromedel.AccessPortal.Web/index.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const apiUrl = process.env.API_URL
+
+const License = function ({ title, description }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </>
+  )
+}
+
 const App = function () {
-  return <div>Hello world</div>
+  const [licenses, setLicenses] = useState([])
+
+  useEffect(() => {
+    const fetchLicenses = async () => {
+      const response = await fetch(`${apiUrl}/license`)
+      const json = await response.json()
+
+      setLicenses(json)
+    }
+
+    fetchLicenses()
+  }, [])
+  
+  return (
+    <div>
+      <h1>Licenses</h1>
+      {licenses.map(license =>
+        <License
+          key={license.title}
+          title={license.title}
+          description={license.description}
+        />)}
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
