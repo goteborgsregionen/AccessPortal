55a54835cb37f33d032c0bea6f9bc0610a08dc78
Adam Näslund <33250901+iteadam@users.noreply.github.com>
2020-10-07 17:22:30 +0200

    Update GR.Laromedel.AccessPortal.Services/Models/ResourceViewModel.cs

--- GR.Laromedel.AccessPortal.Services/Models/ResourceViewModel.cs
+++ GR.Laromedel.AccessPortal.Services/Models/ResourceViewModel.cs
@@ -5,9 +5,9 @@ namespace GR.Laromedel.AccessPortal.Services.Models
 {
     public class ResourceViewModel
     {
-        public string Title {get; set;}
-        public string Description {get; set;}
-        public Uri ImageUri {get;set;}
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Uri ImageUri { get; set; }
         public Uri ResourceUri { get; set; }
         public Dictionary<string,string []> Classifications { get; set; }
     }
