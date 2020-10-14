6972120034a28b900c79a17f675c5a8d7ba5f7cd
Adam Näslund <33250901+iteadam@users.noreply.github.com>
2020-10-14 14:13:50 +0200

    feat: use mock data from Bok system (#4)
    * feat: use mock data from Bok system
    
    * languages to array, better comments

--- GR.Laromedel.AccessPortal.Services/Models/ResourceViewModel.cs
+++ GR.Laromedel.AccessPortal.Services/Models/ResourceViewModel.cs
@@ -6,9 +6,18 @@ namespace GR.Laromedel.AccessPortal.Services.Models
     public class ResourceViewModel
     {
         public string Title { get; set; }
-        public string Description { get; set; }
-        public Uri ImageUri { get; set; }
         public Uri ResourceUri { get; set; }
-        public Dictionary<string,string []> Classifications { get; set; }
+        public string ArticleNumber { get; set; }
+        public string CollectionTitle { get; set; }
+        public string CollectionPartNumber { get; set; } // like "2" or "2/10"
+        public Uri ImageThumbUri { get; set; }
+        public Uri ImageUri { get; set; }
+        public int Edition { get; set; }
+        public string Description { get; set; }
+        public string Contributors { get; set; }
+        public string[] Languages { get; set; }
+        public string SupplierName { get; set; }
+        public string[] Subjects { get; set; }
+        public string[] Stage { get; set; }
     }
 }
