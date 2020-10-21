c03162d23d94f56186b06124c3220f1caf0de4da
Adam Näslund <adam.naslund@iteam.se>
2020-10-21 17:12:36 +0200

    Reuse Container

--- GR.Laromedel.AccessPortal.Web/src/footer.js
+++ GR.Laromedel.AccessPortal.Web/src/footer.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import Container from './general/container';
 
 export default function Footer() {
   return (
     <div className="h-40 pt-6 bg-green-700">
-      <div className="pl-6 w-256 mx-auto">
+      <Container>
         <ul className="text-sm font-bold text-white">
           <li className="mb-2"><a href="about">Om webbplatsen</a></li>
           <li className="mb-2"><a href="privacy">Cookies & persondata</a></li>
           <li className="mb-2"><a href="https://goteborgsregionen.se">Göteborgsregionen</a></li>
         </ul>
-      </div>
+      </Container>
     </div>
   );
 }
