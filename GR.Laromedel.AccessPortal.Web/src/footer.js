ba1e3460faeb7e4bc41829a84b8906cfff8b1e3a
Adam Näslund <adam.naslund@iteam.se>
2020-10-21 18:19:17 +0200

    feat: use react router for linking to /about and /privacy

--- GR.Laromedel.AccessPortal.Web/src/footer.js
+++ GR.Laromedel.AccessPortal.Web/src/footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
     <div className="h-40 pt-6 bg-green-700">
       <div className="pl-6 w-256 mx-auto">
         <ul className="text-sm font-bold text-white">
-          <li className="mb-2"><a href="about">Om webbplatsen</a></li>
-          <li className="mb-2"><a href="privacy">Cookies & persondata</a></li>
+          <li className="mb-2"><Link to="/about">Om webbplatsen</Link></li>
+          <li className="mb-2"><Link to="/privacy">Cookies & persondata</Link></li>
           <li className="mb-2"><a href="https://goteborgsregionen.se">Göteborgsregionen</a></li>
         </ul>
       </div>
