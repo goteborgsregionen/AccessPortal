4e19217eb64880a4f72cd7f8464dd88b88d9dbe7
izabellalarsson <izzwii.izabella@gmail.com>
2020-11-05 10:01:19 +0100

    chore: move pages to pages folder

---
+++ GR.Laromedel.AccessPortal.Web/src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <>
+      <h1 className="text-xl font-bold mb-4">Om Åtkomstportalen</h1>
+      <p>
+        Åtkomstportalen är skapad av{' '}
+        <a className="underline" href="https://goteborgsregionen.se">
+          Göteborgsregionen
+        </a>{' '}
+        för att göra den enkelt för elever och lärare att använda digitala läromedel.
+      </p>
+      <p>
+        Just nu är portalen under utveckling och den kommer förbättras och få flera funktioner
+        framöver.
+      </p>
+    </>
+  );
+}
