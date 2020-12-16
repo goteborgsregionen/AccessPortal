1bf891e34265c101deb9ce0bd2ed6560bd5002fe
Izabella Larsson <izabella.larsson@iteam.se>
2020-12-16 17:03:09 +0100

    check if user is logged in (#19)
    * check if user is logged in
    
    * prettier

--- GR.Laromedel.AccessPortal.Web/src/pages/welcome.js
+++ GR.Laromedel.AccessPortal.Web/src/pages/welcome.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import { oidcConfig } from '../config/oidcconfig';
 
 export default function Welcome() {
+  const key = sessionStorage.getItem(`oidc.user:${oidcConfig.authority}:${oidcConfig.clientId}`);
+  const history = useHistory();
+
+  if (key) history.push('/atkomstportalen');
   return (
     <>
       <h1 className="text-xl font-bold mb-4">Välkommen till Åtkomstportalen</h1>
