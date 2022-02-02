293ad89c2c5769be0230ef1dff68c6ce0b0ab827
henrikjademyr <70258725+henrikjademyr@users.noreply.github.com>
2022-02-02 17:14:39 +0100

    Releases/ver 2 (#42)
    * Upped version
    
    * Fixed build trigger
    
    * Uppded versions
    
    * Fixed REDIRECT_URI
    
    * Upped version
    
    * Fixed bug
    
    * Deploy bug
    
    * Changed from "uuid" to "email"
    
    * Refactored oidcconfig and reverted to uuid
    
    * Fix
    
    * Added debug logging
    
    * Debug
    
    * Debugger 2
    
    * More logging
    
    * Test
    
    * Added Serilog
    
    * Updated debug and claimtype
    
    Co-authored-by: Henrik Jademyr <Henrik.Jademyr@grkom.se>

--- GR.Laromedel.AccessPortal.Api/Controllers/ResourcesController.cs
+++ GR.Laromedel.AccessPortal.Api/Controllers/ResourcesController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using GR.Laromedel.AccessPortal.Services.Services.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace GR.Laromedel.AccessPortal.Api.Controllers
 {
@@ -15,16 +17,18 @@ namespace GR.Laromedel.AccessPortal.Api.Controllers
     public class ResourcesController : ControllerBase
     {
         private readonly IResourceService _resourceService;
+        private readonly ILogger<ResourcesController> _logger;
 
-        public ResourcesController(IResourceService resourceService)
+        public ResourcesController(IResourceService resourceService, ILogger<ResourcesController> logger)
         {
             _resourceService = resourceService;
+            _logger = logger;
         }
 
         [HttpGet]
         public ActionResult Get()
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "uuid").Value;
+            var userId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
 
             if (string.IsNullOrEmpty(userId))
             {
