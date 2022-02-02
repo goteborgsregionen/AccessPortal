9ce0eaf2ec1c9a199eb3a9d0f8b6809bc0d11f7e
Henrik Jademyr <Henrik.Jademyr@grkom.se>
2022-02-02 11:21:01 +0100

    Refactored oidcconfig and reverted to uuid

--- GR.Laromedel.AccessPortal.Api/Controllers/ResourcesController.cs
+++ GR.Laromedel.AccessPortal.Api/Controllers/ResourcesController.cs
@@ -24,7 +24,7 @@ namespace GR.Laromedel.AccessPortal.Api.Controllers
         [HttpGet]
         public ActionResult Get()
         {
-            var userId = User.Claims.FirstOrDefault(c => c.Type == "email").Value;
+            var userId = User.Claims.FirstOrDefault(c => c.Type == "uuid").Value;
 
             if (string.IsNullOrEmpty(userId))
             {
