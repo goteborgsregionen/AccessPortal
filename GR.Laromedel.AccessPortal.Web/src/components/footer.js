bb2126a9fbc5e72d1b31388e590f762aba70a856
TomAndConsid <tomas.andersson@consid.se>
2025-02-13 13:58:51 +0100

    Changed Look Of Welcome Page
    To fix this, I also made some changes to the css structure.
    
    Ref: #20562

--- GR.Laromedel.AccessPortal.Web/src/components/footer.js
+++ GR.Laromedel.AccessPortal.Web/src/components/footer.js
@@ -4,16 +4,16 @@ import Container from './common/container';
 
 export default function Footer() {
   return (
-    <div className="h-40 pt-6 bg-green-700">
+    <div role="footer" className="footer">
       <Container>
-        <ul className="text-sm font-bold text-white">
-          <li className="mb-2">
+        <ul className="footer-links">
+          <li>
             <Link to="/about">Om webbplatsen</Link>
           </li>
-          <li className="mb-2">
+          <li>
             <Link to="/privacy">Cookies & persondata</Link>
           </li>
-          <li className="mb-2">
+          <li>
             <a href="https://goteborgsregionen.se">Göteborgsregionen</a>
           </li>
         </ul>
--- GR.Laromedel.AccessPortal.Web/src/pages/welcome.js
+++ GR.Laromedel.AccessPortal.Web/src/pages/welcome.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { oidcConfig } from '../config/oidcconfig';
+import Login from '../components/login';
 
-export default function Welcome() {
+export default function Welcome() {  
   const key = sessionStorage.getItem(`oidc.user:${oidcConfig.authority}:${oidcConfig.clientId}`);
-  const history = useHistory();
-
+  const history = useHistory();  
   if (key) history.push('/atkomstportalen');
   return (
-    <>
-      <h1 className="text-xl font-bold mb-4">Välkommen till Åtkomstportalen</h1>
+    <div className="login-box">
+      <h1>Välkommen till Åtkomstportalen</h1>
       <p>
         Åtkomstportalen är skapad av{' '}
-        <a className="underline" href="https://goteborgsregionen.se">
+        <a href="https://goteborgsregionen.se">
           Göteborgsregionen
         </a>{' '}
         för att göra den enkelt för elever och lärare att använda digitala läromedel.
       </p>
       <p>För att kunna använda Åtkomstportalen behöver du logga in med ditt skolkonto.</p>
-    </>
-  );
+      <Login />
+    </div>
+  );  
 }
