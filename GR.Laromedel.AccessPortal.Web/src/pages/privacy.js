6c1ebd63008eeca3d363b943d4eba4b782e3625a
TomAndConsid <tomas.andersson@consid.se>
2025-02-19 09:52:58 +0100

    Cleaned Up Html
    Adjusted to have cleaner html with css classes.
    
    Ref: #20563

--- GR.Laromedel.AccessPortal.Web/src/pages/privacy.js
+++ GR.Laromedel.AccessPortal.Web/src/pages/privacy.js
@@ -4,7 +4,7 @@ import CommonInfoBox from '../components/common/CommonInfoBox';
 export default function Privacy() {
   return (
     <CommonInfoBox>
-      <h1 className="text-xl font-bold mb-4">Cookies och persondata</h1>
+      <h1>Cookies och persondata</h1>
       <p>
         Åtkomstportalen använder varken cookies eller persondata på något sätt för tillfället. Det
         kommer däremot att ändras när man faktiskt kan logga in och göra saker. Då kommer det stå
